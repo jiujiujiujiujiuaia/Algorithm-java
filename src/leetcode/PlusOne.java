bfcffa905883f853334b3ebfaa77c8733a39202d
yuchunwei <yuchunwei@hujiang.com>
2018-10-12 09:42:25 +0800

    update:开始图的算法并刷leetcode

---
+++ src/leetcode/PlusOne.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+        int length = digits.length;
+        int last = digits[length-1];
+        if(last == 9){
+            int point = length-1;
+            while(digits[point] == 9){
+                digits[point] = 0;
+                point--;
+                if(point<0){
+                    break;
+                }
+            }
+            if(point>=0){
+                digits[point] = digits[point] + 1;
+            }
+            else {
+                int[] result = new int[length+1];
+                result[0] = 1;
+                for(int i =1;i<length;i++){
+                    result[i]=0;
+                }
+                return result;
+            }
+        }
+        else{
+            digits[length-1] = digits[length-1]+1;
+        }
+        return digits;
+    }
+}
