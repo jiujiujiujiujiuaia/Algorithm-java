8e8cf08d2b4d7eb1b05176b69a7f3fee46003561
jiujiuaia <jiujiuaia@gmail.com>
2019-01-18 22:18:47 +0800

    update：继续数组以及排序的刷题

---
+++ src/leetcode/array/KthLargestElementinanArray.java
@@ -0,0 +1,12 @@
+package leetcode.array;
+
+/**
+ * @Author yuchunwei
+ * leetcode 215
+ */
+public class KthLargestElementinanArray {
+
+    public static void main(String[] args) {
+
+    }
+}   
---
+++ src/leetcode/array/QuickSort.java
@@ -0,0 +1,50 @@
+package leetcode.array;
+
+import edu.princeton.cs.algs4.QuickBentleyMcIlroy;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * @Author yuchunwei
+ * myself
+ */
+public class QuickSort {
+
+    public void quickSort(int[] nums ,int l , int r){
+        if(l >= r )return ;
+        int j = adjArray(nums,l,r);
+        quickSort(nums,l,j-1);
+        quickSort(nums,j+1,r);
+    }
+    private int adjArray(int[] nums,int l ,int r){
+        //定义基准
+        int temp = nums[l];
+        //[l+1 , j] 小于 temp [j + 1 ,i] 大于 temp
+        int j = l ;
+        for(int i = l + 1 ; i <= r ; i++){
+            if(nums[i] < temp) {
+                int x = nums[i] ;
+                nums[i] = nums[j+1] ;
+                nums[j+1] = x ;
+                j++;
+            }
+        }
+        //j的位置就是基准数实际的位置
+        int a = nums[j] ;
+        nums[j] = nums[l];
+        nums[l] = a ;
+        return j ;
+    }
+
+    public static void main(String[] args) {
+        int length = 10;
+        int[] nums = new int[length];
+        for(int i = 0 ; i < length ;i++){
+            nums[i] = (int)(Math.random()*100)+1;
+        }
+        QuickSort quickSort = new QuickSort();
+        quickSort.quickSort(nums,0,length-1);
+        System.out.println(Arrays.toString(nums));
+    }
+}   
