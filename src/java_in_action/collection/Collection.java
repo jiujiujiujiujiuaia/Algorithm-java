5e70fd4824ab861ab0e1d6db61a3d7d98be92018
jiujiuaia <jiujiuaia@gmail.com>
2019-03-01 10:36:18 +0800

    update

--- src/java_in_action/collection/Collection.java
+++ src/java_in_action/collection/Collection.java
@@ -1,21 +1,24 @@
 package java_in_action.collection;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @Author yuchunwei
  */
-public class Lists {
-    public static void main(String[] args) {
-        System.out.println("start");
+public class Collection {
+
+    private static void testList(){
+        System.out.println(" LinkedList start");
         List<Integer> list = new LinkedList<>();
         ((LinkedList<Integer>) list).addFirst(1);
         ((LinkedList<Integer>) list).addFirst(1);
         ((LinkedList<Integer>) list).addFirst(1);
         ((LinkedList<Integer>) list).addFirst(1);
         ((LinkedList<Integer>) list).addFirst(1);
+        list.remove(1);
         System.out.println(" current size " + list.size());
         ((LinkedList<Integer>) list).peek();
         System.out.println("after peek" + list.size());
@@ -24,5 +27,23 @@ public class Lists {
         ((LinkedList<Integer>) list).poll();
         System.out.println("after poll" + list.size());
         System.out.println(list.remove(0));
+    }
+
+    private static void testSet(){
+        Set set = new HashSet();
+        for(int i = 0 ; i < 10 ;i++){
+            set.add(i) ;
+        }
+        for(Object a : set){
+            System.out.println(a);
+        }
+        System.out.println("hashset按照添加顺序保存");
+
+    }
+
+
+
+    public static void main(String[] args) {
+        testSet();
     }
 }
